bbc69c4bac16881587756af151926586eeed55df
Hyojeong <gywjd1701@gmail.com>
2021-11-01 06:22:10 +0900

    add

---
+++ provider/modules/reservation/AddReservation.tsx
@@ -0,0 +1,16 @@
+const Reservation = () => {
+return (
+  <div>
+    <button>1회</button>
+    <button>10회</button>
+    <button>30회</button><br />
+    <input type="text" placeholder="상담 예약자 이름"/><br />
+    <input type="text" placeholder="상담 예약자 연락처"/><br />
+    <input type="text" placeholder="상담 예약자 문의사항"/><br />
+  <button>예약등록</button>
+  </div>
+)
+};
+
+
+export default Reservation;
